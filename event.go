738b75cb7919ea57d7d2d4eb0a2aa59eb9535ee4
pnegre <pere.negre@gmail.com>
2014-08-04 14:34:47 +0200

    Added WaitEvent

--- event.go
+++ event.go
@@ -61,50 +61,56 @@ type EventResize struct {
 	W, H int
 }
 
+// Wait for next event
+func WaitEvent() Event {
+	var cev C.SDL_Event
+	if 0 == C.SDL_WaitEvent(&cev) {
+		return nil
+	}
+	return classifyEvent(&cev)
+}
+
 // Poll for pending envents. Return nil if there is no event available
 func PollEvent() Event {
 	var cev C.SDL_Event
+	if 0 == C.SDL_PollEvent(&cev) {
+		return nil
+	}
+	return classifyEvent(&cev)
+}
 
-	for {
-		if 0 == C.SDL_PollEvent(&cev) {
-			return nil
+func classifyEvent(cev *C.SDL_Event) Event {
+	switch C.getEventType(cev) {
+
+	case C.SDL_QUIT:
+		return new(EventQuit)
+
+	case C.SDL_KEYDOWN:
+		// Ignore repeat key events
+		if C.isKeyRepeat(cev) != 0 {
+			break
 		}
+		kde := new(EventKey)
+		kde.Code = int(C.getKeyCode(cev))
+		kde.Down = true
+		return kde
 
-		switch C.getEventType(&cev) {
+	case C.SDL_KEYUP:
+		kde := new(EventKey)
+		kde.Code = int(C.getKeyCode(cev))
+		kde.Down = false
+		return kde
 
-		case C.SDL_QUIT:
-			return new(EventQuit)
-
-		case C.SDL_KEYDOWN:
-			// Ignore repeat key events
-			if C.isKeyRepeat(&cev) != 0 {
-				break
-			}
-			kde := new(EventKey)
-			kde.Code = int(C.getKeyCode(&cev))
-			kde.Down = true
-			return kde
-
-		case C.SDL_KEYUP:
-			kde := new(EventKey)
-			kde.Code = int(C.getKeyCode(&cev))
-			kde.Down = false
-			return kde
-
-		case C.SDL_WINDOWEVENT:
-			var w, h C.int
-			if 1 == C.getWinData(&cev, &w, &h) {
-				wr := new(EventResize)
-				wr.W = int(w)
-				wr.H = int(h)
-				return wr
-			}
-
-		default:
-			return new(EventUnknown)
+	case C.SDL_WINDOWEVENT:
+		var w, h C.int
+		if 1 == C.getWinData(cev, &w, &h) {
+			wr := new(EventResize)
+			wr.W = int(w)
+			wr.H = int(h)
+			return wr
 		}
 	}
-
+	return new(EventUnknown)
 }
 
 // Process events. Returns true if EventQuit has appeared
