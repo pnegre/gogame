6bb85997d4311f3b5a2a47a4589ab67a178db658
pnegre <pere.negre@gmail.com>
2014-08-08 11:02:45 +0200

    Changed key codes for scancodes

--- event.go
+++ event.go
@@ -9,7 +9,7 @@ int getEventType(SDL_Event *e) {
 }
 
 int getKeyCode(SDL_Event *e) {
-    return e->key.keysym.sym;
+    return e->key.keysym.scancode;
 }
 
 int isKeyRepeat(SDL_Event *e) {
@@ -18,8 +18,9 @@ int isKeyRepeat(SDL_Event *e) {
 
 int isKeyPressed(int kcode) {
     const Uint8 * state = SDL_GetKeyboardState(NULL);
-    int sc = SDL_GetScancodeFromKey(kcode);
-    return state[sc];
+    return state[kcode];
+    // int sc = SDL_GetScancodeFromKey(kcode);
+    // return state[sc];
 }
 
 int getWindowEvent(SDL_Event *e) {
@@ -50,19 +51,39 @@ void getMouseWheel(SDL_Event *e, int *x, int *y) {
 import "C"
 
 const (
-	K_LEFT   = C.SDLK_LEFT
-	K_RIGHT  = C.SDLK_RIGHT
-	K_UP     = C.SDLK_UP
-	K_DOWN   = C.SDLK_DOWN
-	K_SPACE  = C.SDLK_SPACE
-	K_ESC    = C.SDLK_ESCAPE
-	K_RETURN = C.SDLK_RETURN
-	K_P      = C.SDLK_p
-	K_I      = C.SDLK_i
-	K_M      = C.SDLK_m
-	K_Q      = C.SDLK_q
-	K_F      = C.SDLK_f
-	K_T      = C.SDLK_t
+	K_LEFT   = C.SDL_SCANCODE_LEFT
+	K_RIGHT  = C.SDL_SCANCODE_RIGHT
+	K_UP     = C.SDL_SCANCODE_UP
+	K_DOWN   = C.SDL_SCANCODE_DOWN
+	K_SPACE  = C.SDL_SCANCODE_SPACE
+	K_ESC    = C.SDL_SCANCODE_ESCAPE
+	K_RETURN = C.SDL_SCANCODE_RETURN
+	K_A      = C.SDL_SCANCODE_A
+	K_B      = C.SDL_SCANCODE_B
+	K_C      = C.SDL_SCANCODE_C
+	K_D      = C.SDL_SCANCODE_D
+	K_E      = C.SDL_SCANCODE_E
+	K_F      = C.SDL_SCANCODE_F
+	K_G      = C.SDL_SCANCODE_G
+	K_H      = C.SDL_SCANCODE_H
+	K_I      = C.SDL_SCANCODE_I
+	K_J      = C.SDL_SCANCODE_J
+	K_K      = C.SDL_SCANCODE_K
+	K_L      = C.SDL_SCANCODE_L
+	K_M      = C.SDL_SCANCODE_M
+	K_N      = C.SDL_SCANCODE_N
+	K_O      = C.SDL_SCANCODE_O
+	K_P      = C.SDL_SCANCODE_P
+	K_Q      = C.SDL_SCANCODE_Q
+	K_R      = C.SDL_SCANCODE_R
+	K_S      = C.SDL_SCANCODE_S
+	K_T      = C.SDL_SCANCODE_T
+	K_U      = C.SDL_SCANCODE_U
+	K_V      = C.SDL_SCANCODE_V
+	K_W      = C.SDL_SCANCODE_W
+	K_X      = C.SDL_SCANCODE_X
+	K_Y      = C.SDL_SCANCODE_Y
+	K_Z      = C.SDL_SCANCODE_Z
 )
 
 type Event interface{}
