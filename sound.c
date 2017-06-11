6bf7020fc2ec2ddbdf916561e8fa4e03a19b9704
pnegre <pere.negre@gmail.com>
2017-06-11 14:32:25 +0200

    Anem fent

--- sound.c
+++ sound.c
@@ -23,23 +23,23 @@ void setFreq(int id, int freq) {
 
 void audioCallback(void* userdata, Uint8* stream, int len) {
 	int *id = (int*) userdata;
-	float v = vvs[*id];
-	float freq = freqsCallback[*id];
-	float amp = amplitudesCallback[*id];
-	float *data = (float*) stream;
-	int N = (int) (2*FREQUENCY/freq);
-	for(int i=0, j=0; i<len / 4; i++) {
-		data[i] = amp * sin(v * 2 * M_PI / FREQUENCY);
-		if (j > N) {
-			v -= N*freq;
-			j = 0;
-		} else {
-			v += freq;
-			j++;
-		}
-	}
-	vvs[*id] = v;
-	soundGoCallback();
+	// float v = vvs[*id];
+	// float freq = freqsCallback[*id];
+	// float amp = amplitudesCallback[*id];
+	// float *data = (float*) stream;
+	// int N = (int) (2*FREQUENCY/freq);
+	// for(int i=0, j=0; i<len / 4; i++) {
+	// 	data[i] = amp * sin(v * 2 * M_PI / FREQUENCY);
+	// 	if (j > N) {
+	// 		v -= N*freq;
+	// 		j = 0;
+	// 	} else {
+	// 		v += freq;
+	// 		j++;
+	// 	}
+	// }
+	// vvs[*id] = v;
+	soundGoCallback(*id, stream, len);
 }
 
 SDL_AudioDeviceID newAudioDevice() {
--- sound.go
+++ sound.go
@@ -13,15 +13,24 @@ extern SDL_AudioDeviceID newAudioDevice();
 */
 import "C"
 import "errors"
+import "unsafe"
+import "math"
+
+var toneCache = make(map[int]*ToneGenerator)
 
 //export soundGoCallback
-func soundGoCallback() {
-	// fmt.Println("AGoFunction()")
+func soundGoCallback(id int, ptr unsafe.Pointer, len int) {
+	tg := toneCache[id]
+	len /= 4
+	slice := (*[1 << 30]float32)(unsafe.Pointer(ptr))[:len:len]
+	tg.feedSamples(slice)
 }
 
 
 type ToneGenerator struct {
 	dev C.SDL_AudioDeviceID
+	amp float32
+	freq float32
 }
 
 func NewToneGenerator() (*ToneGenerator, error) {
@@ -31,6 +40,7 @@ func NewToneGenerator() (*ToneGenerator, error) {
 	}
 	sd := new(ToneGenerator)
 	sd.dev = dev
+	toneCache[int(sd.dev)] = sd
 	return sd, nil
 }
 
@@ -43,13 +53,34 @@ func (self *ToneGenerator) Stop() {
 }
 
 func (self *ToneGenerator) SetFreq(freq int) {
-	C.setFreq(C.int(self.dev), C.int(freq))
+	self.freq = float32(freq)
+	// C.setFreq(C.int(self.dev), C.int(freq))
 }
 
 func (self *ToneGenerator) SetAmplitude(amp int) {
-	C.setAmplitude(C.int(self.dev), C.int(amp))
+	self.amp = float32(amp)
+	// C.setAmplitude(C.int(self.dev), C.int(amp))
 }
 
 func (self *ToneGenerator) Close() {
 	C.SDL_CloseAudioDevice(self.dev)
+}
+
+func (self *ToneGenerator) feedSamples(data []float32) {
+	var v float32
+	for i:=0; i<len(data); i++ {
+		data[i] = self.amp * float32(math.Sin(float64(v * 2 * math.Pi / 44100)))
+		v += self.freq
+	}
+	//
+	// for(int i=0, j=0; i<len / 4; i++) {
+	// 	data[i] = amp * sin(v * 2 * M_PI / FREQUENCY);
+	// 	if (j > N) {
+	// 		v -= N*freq;
+	// 		j = 0;
+	// 	} else {
+	// 		v += freq;
+	// 		j++;
+	// 	}
+	// }
 }
