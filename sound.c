78d65b024d5b4226d9a7877633cdabcf73b405f6
pnegre <pere.negre@gmail.com>
2017-06-11 12:40:12 +0200

    Separat sound en arxiu .c

---
+++ sound.c
@@ -0,0 +1,65 @@
+#include "SDL.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define MAXTONEGENERATORS 100
+
+int FREQUENCY = 44100;
+
+float vvs[MAXTONEGENERATORS];
+int amplitudesCallback[MAXTONEGENERATORS];
+int freqsCallback[MAXTONEGENERATORS];
+
+void setAmplitude(int id, int amp) {
+	amplitudesCallback[id] = amp;
+}
+
+void setFreq(int id, int freq) {
+	freqsCallback[id] = freq;
+}
+
+void audioCallback(void* userdata, Uint8* stream, int len) {
+	int *id = (int*) userdata;
+	float v = vvs[*id];
+	float freq = freqsCallback[*id];
+	float amp = amplitudesCallback[*id];
+	float *data = (float*) stream;
+	int N = (int) (2*FREQUENCY/freq);
+	for(int i=0, j=0; i<len / 4; i++) {
+		data[i] = amp * sin(v * 2 * M_PI / FREQUENCY);
+		if (j > N) {
+			v -= N*freq;
+			j = 0;
+		} else {
+			v += freq;
+			j++;
+		}
+	}
+	vvs[*id] = v;
+}
+
+SDL_AudioDeviceID newAudioDevice() {
+	int *id = (int*) malloc(sizeof(int));
+    SDL_AudioSpec want, have;
+    SDL_AudioDeviceID dev;
+    SDL_memset(&want, 0, sizeof(want));
+    want.freq = FREQUENCY;
+    want.format = AUDIO_F32;
+    want.channels = 1;
+    want.samples = 4096;
+    want.callback = audioCallback;
+	want.userdata = id;
+
+    dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
+    if (dev == 0) {
+        SDL_Log("Failed to open audio: %s", SDL_GetError());
+    }
+	if (dev >= MAXTONEGENERATORS) {
+		SDL_Log("Error: MAXTONEGENERATORS reached!!");
+		return 0;
+	}
+
+	*id = dev;
+    return dev;
+}
--- sound.go
+++ sound.go
@@ -4,62 +4,11 @@ package gogame
 #cgo pkg-config: sdl2
 #cgo LDFLAGS: -lm
 #include "SDL.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
 
-#define MAXTONEGENERATORS 100
-
-int FREQUENCY = 44100;
-
-float vvs[MAXTONEGENERATORS];
-int amplitudesCallback[MAXTONEGENERATORS];
-int freqsCallback[MAXTONEGENERATORS];
-
-void audioCallback(void* userdata, Uint8* stream, int len) {
-	int *id = (int*) userdata;
-	float v = vvs[*id];
-	float freq = freqsCallback[*id];
-	float amp = amplitudesCallback[*id];
-	float *data = (float*) stream;
-	int N = (int) (2*FREQUENCY/freq);
-	for(int i=0, j=0; i<len / 4; i++) {
-		data[i] = amp * sin(v * 2 * M_PI / FREQUENCY);
-		if (j > N) {
-			v -= N*freq;
-			j = 0;
-		} else {
-			v += freq;
-			j++;
-		}
-	}
-	vvs[*id] = v;
-}
-
-SDL_AudioDeviceID newAudioDevice() {
-	int *id = (int*) malloc(sizeof(int));
-    SDL_AudioSpec want, have;
-    SDL_AudioDeviceID dev;
-    SDL_memset(&want, 0, sizeof(want));
-    want.freq = FREQUENCY;
-    want.format = AUDIO_F32;
-    want.channels = 1;
-    want.samples = 4096;
-    want.callback = audioCallback;
-	want.userdata = id;
-
-    dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
-    if (dev == 0) {
-        SDL_Log("Failed to open audio: %s", SDL_GetError());
-    }
-	if (dev >= MAXTONEGENERATORS) {
-		SDL_Log("Error: MAXTONEGENERATORS reached!!");
-		return 0;
-	}
-
-	*id = dev;
-    return dev;
-}
+extern void setAmplitude(int id, int amp);
+extern void setFreq(int id, int freq);
+extern void audioCallback(void* userdata, Uint8* stream, int len);
+extern SDL_AudioDeviceID newAudioDevice();
 
 */
 import "C"
@@ -88,11 +37,11 @@ func (self *ToneGenerator) Stop() {
 }
 
 func (self *ToneGenerator) SetFreq(freq int) {
-	C.freqsCallback[self.dev] = C.int(freq)
+	C.setFreq(C.int(self.dev), C.int(freq))
 }
 
 func (self *ToneGenerator) SetAmplitude(amp int) {
-	C.amplitudesCallback[self.dev] = C.int(amp)
+	C.setAmplitude(C.int(self.dev), C.int(amp))
 }
 
 func (self *ToneGenerator) Close() {
