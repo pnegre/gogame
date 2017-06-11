038c8a5e6c3d4412a54edf6dc79723af604ad013
pnegre <pere.negre@gmail.com>
2017-06-11 15:18:42 +0200

    Anem fent

--- tone.go
+++ tone.go
@@ -2,12 +2,8 @@ package gogame
 
 /*
 #cgo pkg-config: sdl2
-#cgo LDFLAGS: -lm
 #include "SDL.h"
 
-extern void setAmplitude(int id, int amp);
-extern void setFreq(int id, int freq);
-extern void audioCallback(void* userdata, Uint8* stream, int len);
 extern SDL_AudioDeviceID newAudioDevice(int frequency);
 extern void closeAudioDevice(SDL_AudioDeviceID id);
 
@@ -25,6 +21,11 @@ var toneCache = make(map[int]*ToneGenerator)
 func soundGoCallback(id int, ptr unsafe.Pointer, len int) {
 	tg := toneCache[id]
 	len /= 4
+	// To create a Go slice backed by a C array
+	// (without copying the original data), one needs to acquire this length
+	// at runtime and use a type conversion to a pointer to a very big array and then
+	// slice it to the length that you want (also remember to set
+	// the cap if you're using Go 1.2 or later)
 	slice := (*[1 << 30]float32)(unsafe.Pointer(ptr))[:len:len]
 	tg.feedSamples(slice)
 }
@@ -35,7 +36,7 @@ type ToneGenerator struct {
 	freq   float32
 	v      float32
 	period int
-	j int
+	j      int
 }
 
 func NewToneGenerator() (*ToneGenerator, error) {
@@ -74,7 +75,7 @@ func (self *ToneGenerator) Close() {
 }
 
 func (self *ToneGenerator) feedSamples(data []float32) {
-	for i:= 0; i < len(data); i++ {
+	for i := 0; i < len(data); i++ {
 		data[i] = self.amp * float32(math.Sin(float64(self.v*2*math.Pi/FREQUENCY)))
 		if self.j > self.period {
 			self.v -= float32(self.period) * self.freq
