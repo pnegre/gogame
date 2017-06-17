919d708d743c0bc99c07497cd16eb01ffe548d13
pnegre <pere.negre@gmail.com>
2017-06-17 22:10:31 +0200

    Eliminat tone generator

--- tone.go
+++ tone.go
@@ -6,23 +6,20 @@ package gogame
 
 extern SDL_AudioDeviceID newAudioDevice(int frequency);
 extern void closeAudioDevice(SDL_AudioDeviceID id);
+extern void mixAudio(void *src, void* dst, int len);
 
 */
 import "C"
 import "errors"
 import "unsafe"
-// import "math"
-import "math/rand"
 
-const FREQUENCY = 44100
-const INVFREQUENCY = float32(1) / float32(FREQUENCY)
-
-var toneCache = make(map[int]*ToneGenerator)
 
 const (
 	GENERATOR_TYPE_TONE = iota
 	GENERATOR_TYPE_NOISE
 )
+
+var theCallback func([]float32)
 
 //export soundGoCallback
 func soundGoCallback(id int, ptr unsafe.Pointer, len int) {
@@ -33,106 +30,43 @@ func soundGoCallback(id int, ptr unsafe.Pointer, len int) {
 	// slice it to the length that you want (also remember to set
 	// the cap if you're using Go 1.2 or later)
 	slice := (*[1 << 30]float32)(ptr)[:len:len]
-	toneCache[id].feedSamples(slice)
-}
-
-type ToneGenerator struct {
-	genType int
-	dev     C.SDL_AudioDeviceID
-	amp     float32
-	freq    float32
-	v       float32
-	period  int
-	j       int
-	count int
-}
-
-func NewToneGenerator(genType int) (*ToneGenerator, error) {
-	dev := C.newAudioDevice(FREQUENCY)
-	if dev == 0 {
-		return nil, errors.New("Can't open tone generator")
+	if theCallback != nil {
+		theCallback(slice)
 	}
-	sd := new(ToneGenerator)
+}
+
+func RegisterSoundCallback(fnc func([]float32)) {
+	theCallback = fnc
+}
+
+// func MixAudio(src unsafe.Pointer, dst unsafe.Pointer, lg int) {
+// 	C.mixAudio(src, dst, C.int(lg))
+// }
+
+type AudioDevice struct {
+	dev       C.SDL_AudioDeviceID
+	frequency int
+}
+
+func NewAudioDevice(frequency int) (*AudioDevice, error) {
+	dev := C.newAudioDevice(C.int(frequency))
+	if dev == 0 {
+		return nil, errors.New("Can't open audio device")
+	}
+	sd := new(AudioDevice)
 	sd.dev = dev
-	sd.genType = genType
-	toneCache[int(sd.dev)] = sd
+	sd.frequency = frequency
 	return sd, nil
 }
 
-func (self *ToneGenerator) Start() {
+func (self *AudioDevice) Start() {
 	C.SDL_PauseAudioDevice(self.dev, 0)
 }
 
-func (self *ToneGenerator) Stop() {
+func (self *AudioDevice) Stop() {
 	C.SDL_PauseAudioDevice(self.dev, 1)
 }
 
-func (self *ToneGenerator) SetFreq(freq float32) {
-	self.freq = freq
-	self.period = int(0x10000 * self.freq / FREQUENCY)
-}
-
-func (self *ToneGenerator) SetAmplitude(amp float32) {
-	self.amp = amp
-}
-
-func (self *ToneGenerator) Close() {
+func (self *AudioDevice) Close() {
 	C.closeAudioDevice(self.dev)
-	delete(toneCache, int(self.dev))
-}
-
-func (self *ToneGenerator) feedSamples(data []float32) {
-	if self.genType == GENERATOR_TYPE_TONE {
-
-		/*
-
-				Tret de EMULIB
-
-
-				if(WaveCH[J].Freq>=SndRate/2) break;
-		          K=0x10000*WaveCH[J].Freq/SndRate;
-		          L1=WaveCH[J].Count;
-
-				  for(I=0;I<Samples;I++,L1+=K)
-		          {
-		            L2 = L1+K;
-		            A1 = L1&0x8000? 127:-128;
-		            if((L1^L2)&0x8000)
-		              A1=A1*(0x8000-(L1&0x7FFF)-(L2&0x7FFF))/K;
-		            Wave[I]+=A1*V;
-		          }
-		          WaveCH[J].Count=L1&0xFFFF;
-
-		*/
-
-		K := int(0x10000*self.freq/FREQUENCY);
-		L1:=self.count;
-		var A1 int
-		for i := 0; i < len(data); i, L1 = i+1, L1+K {
-			L2 := L1+K;
-			if L1&0x8000 != 0 {
-				A1 = 127
-			} else {
-				A1 = -128
-			}
-			if((L1^L2)&0x8000 != 0) {
-				A1=A1*(0x8000-(L1&0x7FFF)-(L2&0x7FFF))/K;
-			}
-			data[i]=float32(A1)*self.amp;
-
-			// data[i] = self.amp * float32(math.Sin(float64(self.v*2*math.Pi*self.freq)))
-			// self.v += INVFREQUENCY
-			// if self.j > self.period {
-			// 	self.v -= float32(self.j) * INVFREQUENCY
-			// 	self.j = 0
-			// } else {
-			// 	self.j++
-			// }
-		}
-		self.count = L1&0xFFFF
-	} else if self.genType == GENERATOR_TYPE_NOISE {
-		for i := 0; i < len(data); i++ {
-			data[i] = self.amp * rand.Float32()
-		}
-	}
 }
